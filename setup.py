5dda938378c93cbb6601164faf58b14e4dcdc26b
Edgar454 <mevaed4@gmail.com>
2024-12-20 21:41:04 +0100

    Added MANIFEST

--- setup.py
+++ setup.py
@@ -1,17 +1,20 @@
 from setuptools import setup, find_packages
 
+from setuptools import setup, find_packages
+
 setup(
     name="rag-evaluator",          
     version="0.1.0",              
     author="MEVA'A EDGAR",            
     author_email="mevaed4@gmail.com",  
     description="A utility for evaluating Retrieval-Augmented Generation models.",
-    url="https://github.com/edgar454/rag-evaluator",  
+    url="https://github.com/edgar454/rag_evaluator",  
     packages=find_packages(),     
     install_requires=open("requirements.txt").readlines(),  
     include_package_data=True,
     package_data={
-        "rag_evaluator": ["rag_evaluator/data/*.csv", "rag_evaluator/data/*.pkl"],  
+        "rag_evaluator": ["data/*.csv", "data/*.pkl"],  # Paths relative to the `rag_evaluator` package
     },
     python_requires=">=3.8",       
 )
+
